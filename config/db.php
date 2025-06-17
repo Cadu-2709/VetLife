dd0b171f060bef9ad9747eb03eb5d58658c305d0
maiconkl <maiconklitzke3@gmail.com>
2025-06-16 21:42:45 -0300

    Estrutura do projeto

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=vetlife_db',
+    'username' => 'postgres',
+    'password' => 'postgres', // <-- COLOQUE SUA SENHA AQUI
+    'charset' => 'utf8',
+];
